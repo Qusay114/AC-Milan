29405044f1e1fb9e25127b0ba8e32e795fee0bd9
Qusay Mohammed Al-Amarat <qusayamarat@gmail.com>
2021-03-12 15:55:27 +0200

    change the header elements

---
+++ app.js
@@ -0,0 +1,36 @@
+var newsValue       ="test";
+var watchValue      ="test";
+var tacticsValue    ="test";
+var scheduleValue   ="test";
+var aboutValue      ="test";
+
+function newsFunction()
+{
+    document.getElementById("rightChild").innerHTML = newsValue;
+    
+}
+
+function watchFunction()
+{
+    document.getElementById("rightChild").innerHTML = watchValue;
+    
+}
+
+function tacticsFunction()
+{
+    document.getElementById("rightChild").innerHTML = tacticsValue;
+    
+}
+
+function scheduleFunction()
+{
+    document.getElementById("rightChild").innerHTML = scheduleValue;
+    
+}
+
+function aboutFunction()
+{
+    document.getElementById("rightChild").innerHTML = aboutValue;
+    
+}
+
